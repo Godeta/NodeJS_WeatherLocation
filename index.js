214fe9fa9026834296f8dbd06a38bd3d06166eda
Godeta <Godeta61@gmail.com>
2020-04-19 18:54:10 +0200

    Création du serveur sur le port 3000 qui fonctionne, acceuil + dossier geolocalisation

---
+++ index.js
@@ -0,0 +1,10 @@
+//index.js est la partie orienté serveur tandis que le html ou d'autres javascript seront les parties clients
+const express = require("express");
+const app = express();
+app.listen(3000, () => console.log("Listening at 3000"));
+app.use(express.static("public"));
+
+//se déclenche si je fait "node index.js" dans le cmd
+/*objectifs :  1) lorsque l'on accède à la page, afficher tout le html,css,javascript comme normal
+2) recevoir des informations et les enregistrer dans une base de donnée
+3) renvoyer des informations de la base de données */
