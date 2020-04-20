86754005ec6ad4cdb66bd32c3a1c471e4918b965
Godeta <Godeta61@gmail.com>
2020-04-20 17:04:17 +0200

    Ajout d'un peu de CSS et de classes dans le javascript pour décorer la page requête

--- public/geolocalisation/clientViewDB.js
+++ public/geolocalisation/clientViewDB.js
@@ -3,17 +3,19 @@ getData();
 async function getData() {
     const response = await fetch("/api"); //on peut réutiliser api car l'autre fetch api était un POST et ceci est un GET donc deux routes différentes
     const data = await response.json();
-    for (item of data) {
+    for (item of data) { //création de différents div
         const root = document.createElement("div");
+        root.className = "pack";
         const nom = document.createElement("div");
-        nom.textContent = "nom : ${item.nom}";
+        nom.className = "nom";
         const geo = document.createElement("div");
-        geo.textContent = "nom : ${item.lat}°,${item.lon}°,${item.accu}°";
-
         const date = document.createElement("div");
-        const dateString = new Date(item.timestamp).toLocaleString();
-        date.textContent = dateString;
-        root.append(nom, geo, date);
-        document.body.append(root);
+        nom.textContent = 'nom : ' + item.nom; //nom
+        geo.textContent = "localisation : " + item.lat + "°, " + item.lon + ", " + item.accu + "°"; //affichage de la localisation
+
+        const dateString = new Date(item.timestamp).toLocaleString(); //date remise sous forme de String
+        date.textContent = "date : " + dateString;
+        root.append(nom, geo, date); //on ajoute les différents div dans le div root
+        document.body.append(root); //on ajoute le div root au body
     }
 }
