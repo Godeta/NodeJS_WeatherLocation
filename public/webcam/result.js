043479183ba67efbdafcaebab3cba80a94262240
Godeta <Godeta61@gmail.com>
2020-04-26 13:10:21 +0200

    Ajout d'une méthode de trie pour les images webcam

--- public/webcam/result.js
+++ public/webcam/result.js
@@ -1,6 +1,33 @@
 //code executé
 getData();
 
+const selfies = [];
+//si on choisi de trier par temps
+document.getElementById('time').addEventListener('click', event => {
+    sortData((a, b) => b.time - a.time);
+});
+//tri par nom
+document.getElementById('nom').addEventListener('click', event => {
+    sortData((a, b) => { //trie les données a,b par nom croissant
+        if (b.nom > a.nom) return -1;
+        else return 1;
+    });
+});
+//trie les données
+function sortData(compare) {
+    //retire l'élement html
+    for (let item of selfies) {
+        item.elt.remove();
+    }
+    //les tries en fonction de la méthode de comparaison
+    selfies.sort(compare);
+    //les affiche
+    for (let item of selfies) {
+        document.body.append(item.elt);
+    }
+}
+
+//récupère les données et les affiche
 async function getData() {
     const response = await fetch("/api"); //on peut réutiliser api car l'autre fetch api était un POST et ceci est un GET donc deux routes différentes
     const data = await response.json();
@@ -18,6 +45,12 @@ async function getData() {
         const dateString = new Date(item.timestamp).toLocaleString(); //date remise sous forme de String
         date.textContent = "date : " + dateString;
         root.append(nom, webcam, date); //on ajoute les différents div dans le div root
+        //ajoute au tableau de selfies les élements par lequel on va pouvoir trier
+        selfies.push({
+            elt: root,
+            time: item.timestamp,
+            nom: item.nom
+        });
         document.body.append(root); //on ajoute le div root au body
     }
 }
